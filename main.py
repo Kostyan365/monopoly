5e24f732a079f65800c013dd34fe27642d621ea1
Your Name <KostyaGoruliko365@yandex.ru.com>
2022-01-17 17:02:37 +0300

    4 коммит

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+import time
+
 import pygame
 import os
 import sys
 import ctypes
 import random
 import sqlite3
-list_sound=['brosok-igralnyih-kostey','brosok-igralnyih-kostey-25740','igralnaya-kost-upala','katyatsya-po-stolu',
-            'kubiki-razletelis','odin-kubik-brosili-na-stol','zvuk-brosaniya-igralnyih-kostey-2-25771']
+from collections import deque
+list_sound = ['brosok-igralnyih-kostey', 'brosok-igralnyih-kostey-25740', 'igralnaya-kost-upala', 'katyatsya-po-stolu',
+              'kubiki-razletelis', 'odin-kubik-brosili-na-stol', 'zvuk-brosaniya-igralnyih-kostey-2-25771']
 user32 = ctypes.windll.user32
 user32.SetProcessDPIAware()
 pygame.mixer.pre_init(44100, -16, 1, 512)  # важно прописать до pygame.init()
@@ -16,10 +19,13 @@ screen = pygame.display.set_mode(res)  # открывает окно
 width = screen.get_width()  # ширина экрана
 height = screen.get_height()  # высота экрана
 clock = pygame.time.Clock()
-FPS = 50
+FPS = 15
 # основной персонаж
-player = None
-
+pl1 = ['player1/player1_1.png', 'player1/player1_2.png', 'player1/player1_3.png', 'player1/player1_4.png',
+       'player1/player1_5.png', 'player1/player1_6.png']
+pl2 = ['player2/player2_1.png', 'player2/player2_2.png', 'player2/player2_3.png', 'player2/player2_4.png',
+       'player2/player2_5.png', 'player2/player2_6.png']
+list_card = []
 # группы спрайтов
 all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
@@ -31,7 +37,6 @@ tile_height = 70
 def load_img(name, colorkey=None):
     """Функция для удобства загрузки изображений"""
     fullname = os.path.join('data', name)
-    # если файл не существует, то выходим
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
@@ -46,105 +51,78 @@ def load_img(name, colorkey=None):
     return image
 
 
-player_image = load_img('mario.png')
-
-
 def terminate():
     """Функция выхода"""
     pygame.quit()
     sys.exit()
 
 
-def load_level(filename):
-    filename = "data/" + filename
-    # читаем уровень, убирая символы перевода строки
-    with open(filename, 'r') as mapFile:
-        level_map = [line.strip() for line in mapFile]
-    # и подсчитываем максимальную длину
-    max_width = max(map(len, level_map))
-    # дополняем каждую строку пустыми клетками ('.')
-    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
-
-
-"""tile_images = {
-    '#': load_img('atlas.jpg'),
-    '.': load_img('grass.png'),
-    'q': load_img('1/start1.jpg'),
-    'w': load_img('1/start2.jpg'),
-    'e': load_img('1/start3.jpg'),
-    'r': load_img('1/start4.jpg'),
-    't': load_img('11/prisoner1.jpg'),
-    'y': load_img('11/prisoner2.jpg'),
-    'u': load_img('11/prisoner3.jpg'),
-    'i': load_img('11/prisoner4.jpg'),
-
-    'o': load_img('parking1.jpg'),
-    'p': load_img('parking2.jpg'),
-    'a': load_img('parking3.jpg'),
-    's': load_img('parking4.jpg'),
-
-    'd': load_img('31/arest1.jpg'),
-    'f': load_img('31/arest2.jpg'),
-    'g': load_img('31/arest3.jpg'),
-    'h': load_img('31/arest4.jpg'),
-    'j': load_img('8/shans1.jpg'),
-    'k': load_img('8/shans2.jpg'),
-    'l': load_img('shans3.jpg'),
-    ';': load_img('shans4.jpg'),
-    'z': load_img('3\kazna1.jpg'),
-    'x': load_img('3\kazna2.jpg')
-}"""
+class Card:
+    def __init__(self, *args):
+        i = args
+        self.id = i[0]
+        self.name = i[1]
+        self.type_card = i[2]
+        self.status = i[3]
+        self.coords = eval(i[4])
+        self.color = i[5]
+        self.zalog = i[6]
+        self.renta1 = i[7]
+        self.renta2 = i[8]
+        self.renta3 = i[9]
+        self.renta4 = i[10]
+        self.renta5 = i[11]
+        self.renta6 = i[12]
+        self.house = i[13]
+        self.hotel = i[14]
+        self.prise = i[15]
+        self.sprite = i[16]
 
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, tile_type, pos_x, pos_y):
+    def __init__(self, image, pos_x, pos_y):
         super().__init__(tiles_group, all_sprites)
-        self.image = tile_images[tile_type]
+        self.image = image
         self.rect = self.image.get_rect().move(tile_width * pos_x + (width / 2 - 455), tile_height * pos_y + 100)
-
-conn = sqlite3.connect('data/database/cards.db')
-cur = conn.execute("SELECT * FROM tale")
-z = cur.fetchall()
-conn.close()
-
-
-class Cards:
-    def __init__(self, **kwargs):
-        pass
-
-for _ in z:
-    print(_)
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y):
+    def __init__(self, pos_x, pos_y, player_image):
         super().__init__(player_group, all_sprites)
-        self.image = player_image
-
-        self.rect = self.image.get_rect().move(tile_width * pos_x + 15, tile_height * pos_y + 5)
+        self.images = player_image
+        self.index = 0
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect().move(tile_width * pos_x + (width / 2 - 455) + 15, tile_height * pos_y + 105)
         self.pos = (pos_x, pos_y)
+
+    def update(self):
+        """Mетод, анимирует спрайт"""
+        self.index += 1
+        if self.index >= len(self.images):
+            self.index = 0
+        self.image = self.images[self.index]
 
     def move(self, x, y):
         self.pos = (x, y)
-        self.rect = self.image.get_rect().move(tile_width * self.pos[0] + 15, tile_height * self.pos[1] + 5)
+        self.rect = self.image.get_rect().move(tile_width * self.pos[0] + (width / 2 - 455) + 15,
+                                               tile_height * self.pos[1] + 105)
 
 
-def generate_level(level):
-    new_player, x, y = None, None, None
-    for y in range(len(level)):
-        for x in range(len(level[y])):
-            if level[y][x] == '@':
-                Tile('q', x, y)
-                new_player = Player(x, y)
-            else:
-                Tile(level[y][x], x, y)
-    # вернем игрока, а также размер поля в клетках
-    return new_player, x, y
+def generate_level():
+    conn = sqlite3.connect('data/database/cards.db')
+    cur = conn.execute("SELECT * FROM tale")
+    z = cur.fetchall()
+    conn.close()
+    for data in (z):
+        list_card.append(Card(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9],
+                              data[10], data[11], data[12], data[13], data[14], data[15],
+                              [Tile(load_img(f'{data[0]}/img{enum}.jpg'), *_) for enum, _ in
+                               enumerate(eval(data[4]), start=1)]))
 
 
 def start_screen():
     pygame.mixer.music.load("3d20874f20174bd.mp3")
-    pygame.mixer.music.play(-1)
+    # pygame.mixer.music.play(-1)
     fon = pygame.transform.scale(load_img('fon.jpg'), (width, height))
     screen.blit(fon, (0, 0))
 
@@ -197,21 +175,25 @@ def start_screen():
         pygame.display.flip()
         clock.tick(FPS)
 
-
-start_screen()
-level_map = load_level('map.map')
-player, max_x, max_y = generate_level(level_map)
-print(player)
+def generate_player(data):
+    images = [load_img(i) for i in data]
+    return images
 
 
 def dice():
-
-    z=random.choice(list_sound)
-
+    z = random.choice(list_sound)
     s_catch = pygame.mixer.Sound(f'sound/{z}.ogg')
     s_catch.play()
 
-class Board:
+
+
+
+
+
+
+
+
+"""class Board:
     # создание поля
     def __init__(self, width, height):
         self.width = width
@@ -254,31 +236,59 @@ class Board:
         if 0 <= ceil_x < self.width and 0 <= ceil_y < self.height:
             return ceil_x, ceil_y
         return None
-
     def on_click(self, ceil):
-        print(ceil)
+        print(ceil)"""
+start_screen()
+generate_level()
+player1 = Player(11, 11, generate_player(pl1))
+player2 = Player(11, 12,generate_player(pl2))
+print(player1.pos)
+print(player2.pos)
+
+
+
+
+
+
+
+
+rand = 6
+d = deque(list(range(0, 40)))
 
 dice()
 fon = pygame.transform.scale(load_img('fon2.jpg'), (width, height))
+# board = Board(13, 13)
+# board.set_view(width / 2 - 455, 100, 70)
+# движение персонажа
 
-board = Board(13, 13)
-board.set_view(width / 2 - 455, 100, 70)
+
+
 running = True
 while running:
     mouse = pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            board.get_click(event.pos)
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+
+                for i in range(6):
+                    z = d.popleft()
+                    _index = d[0]
+                    print(list_card[_index].coords[0])
+                    x, y = list_card[_index].coords[0]
+                    player1.move(x, y)
+                    d.append(z)
+        # if event.type == pygame.MOUSEBUTTONDOWN:
+        # board.get_click(event.pos)
         if width / 2 - 100 <= mouse[0] <= width / 2 + 100 and height / 1.5 + 60 <= mouse[1] <= height / 1.5 + 60 + 40:
             terminate()  # Если мышь нажала на кнопку игра прекращена
-
     screen.blit(fon, (0, 0))
-
+    # screen.fill(pygame.Color("black"))
     tiles_group.draw(screen)
-    board.render(screen)
-    Tile('q', 11, 1)
+    player_group.update()
+    player_group.draw(screen)
+    # board.render(screen)
+    clock.tick(FPS)
     pygame.display.flip()
-
 pygame.quit()
